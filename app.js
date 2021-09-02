da4003483e886427931baeb2010730e3fc6eceea
Bably <bablyd15@gmail.com>
2021-09-02 21:38:10 +0530

    todo list

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+todoButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("submitted");
+});
